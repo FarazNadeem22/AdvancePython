a66aa375e26b564f236c05d5b094895ac2608a7c
Faraz Nadeem <muhammad.ali.faraz@gmail.com>
2023-02-05 22:42:19 -0500

    Some commit

--- requests/main.py
+++ requests/main.py
@@ -1,1 +1,13 @@
 # Request Library in Python
+# url: https://www.youtube.com/watch?v=Xi1F2ZMAZ7Q
+import requests
+
+
+params ={
+    "name": "Faraz",
+    "age": 22
+}
+
+response = requests.get("https://httpbin.org/get", params=params)
+
+print(response.status_code,"\n",response.text,"\n", response.json())
