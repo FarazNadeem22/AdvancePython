a48c4f496d73486fe6127be028d79feaad590535
Faraz Nadeem <66399838+FarazNadeem22@users.noreply.github.com>
2023-12-11 15:26:23 -0500

    Update classicThreading.py
    commends added

--- threading/classicThreading.py
+++ threading/classicThreading.py
@@ -1,13 +1,25 @@
 import time
 import threading 
 
+# Function to simulate a task
 def worker(name):
-    time.sleep(1)
-    print(f"{name} Completed! Yay")
+    time.sleep(1)  # Simulate task delay of 1 second
+    print(f"{name} Completed! Yay")  # Print completion message with the provided name
 
+# Create two threads for concurrent execution
 
+# Incorrect usage: Invoking worker function immediately and assigning the result to the target of threadOne
+# As a result, worker function is executed in the main thread rather than in a separate thread
 threadOne = threading.Thread(target=worker("Thread two"))
-threadTwo =threading.Thread(target=worker("Thread one"))
 
+# Incorrect usage: Invoking worker function immediately and assigning the result to the target of threadTwo
+# As a result, worker function is executed in the main thread rather than in a separate thread
+threadTwo = threading.Thread(target=worker("Thread one"))
+
+# Start the threads for concurrent execution
+
+# Start the threadTwo (Incorrectly created)
 threadTwo.start()
+
+# Start the threadOne (Incorrectly created)
 threadOne.start()
